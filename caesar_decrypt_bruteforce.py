e9edaf63132b2977e8e1e86c1bbf3a164bf863f2
S-Liedtke <sandra-liedtke@online.de>
2023-01-14 18:42:45 +0100

    Code cleanup

--- caesar_decrypt_bruteforce.py
+++ caesar_decrypt_bruteforce.py
@@ -8,21 +8,19 @@ with open('language_support/text_translations.json', 'r') as translations:
 # per default German (de) and English (en) are available, but feel free to add more translations to the text_translations.json
 language_code = "de"
 
-
 def caesar_bruteforce():
     encrypted_text = input(CONFIG['caesar_input'][language_code])
     dict = {
         'A':0 , 'B':1, 'C':2,'D':3, 'E':4, 'F':5, 'G':6, 'H':7, 'I':8, 'J':9, 'K':10, 'L':11, 'M':12, 'N':13, 'O':14, 'P':15, 'Q':16, 'R':17, 'S':18, 'T':19, 'U':20, 'V':21, 'W':22, 'X':23, 'Y':24, 'Z':25
         }
-    keys = dict.keys()
-    # loopthrough all possible keys 0-25 (alphabeth without Umlaute)
+    # loop through all possible keys 0-25 (alphabeth without Umlaute)
     key = 0
-    for key in range(0, 25):
+    while key < 25:
         clear = ""
         for char in encrypted_text.upper():
             if not char in [" ", ",", ".", "!","?", "(", ")", "Ä", "Ö", "Ü"]:
                 current = str(dict[char])
-                clear += (list(keys))[(int(current) - key) % 26]
+                clear += (list(dict.keys()))[(int(current) - key) % 26]
             else:
                 clear += char
         print(CONFIG['caesar_print_01'][language_code], key, CONFIG['caesar_print_02'][language_code], clear)
